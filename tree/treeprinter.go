573678f4279c0a1540419b6016aad887c1e641c6
Ozoniuss <qscxzsewqazxcde@gmail.com>
2026-08-29 19:37:51 +0300

    Remove unused file

--- tree/treeprinter.go
+++
@@ -1,6 +0,0 @@
-/*
-Complete port of https://github.com/billvanyo/tree_printer/blob/master/src/main/java/tech/vanyo/treePrinter/TreePrinter.java
-to Go. This is not my original implementation.
-*/
-
-package tree
