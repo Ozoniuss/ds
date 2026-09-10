dfebf770d687bb912f27402ac76780c048813c3a
Ozoniuss <qscxzsewqazxcde@gmail.com>
2026-09-10 10:53:36 +0300

    Exhaustive formats with both print versions

--- rbtree/cmd/generateExhaustiveFormat/main.go
+++ rbtree/cmd/generateExhaustiveFormat/main.go
@@ -1,10 +1,9 @@
 // This file is AI-generated.
 //
-// go run ./rbtree/cmd/generateExhaustiveFormat -n=6 -out=rbtree/testdata/exhaustiveFormat.txt
+// go run ./rbtree/cmd/generateExhaustiveFormat
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -12,17 +11,17 @@ import (
 	"github.com/Ozoniuss/ds/rbtree"
 )
 
-func permute(values []int) [][]int {
+func permute(values []string) [][]string {
 	if len(values) == 0 {
-		return [][]int{{}}
+		return [][]string{{}}
 	}
-	var result [][]int
+	var result [][]string
 	for i := range values {
-		rest := make([]int, 0, len(values)-1)
+		rest := make([]string, 0, len(values)-1)
 		rest = append(rest, values[:i]...)
 		rest = append(rest, values[i+1:]...)
 		for _, p := range permute(rest) {
-			perm := append([]int{values[i]}, p...)
+			perm := append([]string{values[i]}, p...)
 			result = append(result, perm)
 		}
 	}
@@ -30,41 +29,55 @@ func permute(values []int) [][]int {
 }
 
 func main() {
-	// n := flag.Int("n", 6, "length of the list [1..n] to permute")
-	out := flag.String("out", "rbtree/testdata/exhaustiveFormat.txt", "output file path")
-	flag.Parse()
 
-	// values := make([]int, *n)
-	// for i := range values {
-	// 	values[i] = i + 1
-	// }
-	values := []int{352345623, 4324232, 21, 4354, 19990593303, 32}
+	opts := []struct {
+		out   string
+		shape int
+	}{
+		{
+			out:   "rbtree/testdata/exhaustiveFormat1.txt",
+			shape: rbtree.SHAPE_TREE,
+		},
+		{
+			out:   "rbtree/testdata/exhaustiveFormat2.txt",
+			shape: rbtree.SHAPE_SQUARE,
+		},
+	}
+
+	values := []string{"552345623", "6324232", "2", "4354", "1999059330313454643", "32"}
 
 	perms := permute(values)
 
-	var sb strings.Builder
-	for _, perm := range perms {
-		strs := make([]string, len(perm))
-		for i, v := range perm {
-			strs[i] = fmt.Sprintf("%d", v)
-		}
-		fmt.Fprintf(&sb, "input: [%s]\n", strings.Join(strs, ","))
+	for _, opt := range opts {
 
-		tr := rbtree.NewRBT[int]()
-		for _, v := range perm {
-			tr.Insert(v)
-		}
+		out := opt.out
+		shape := opt.shape
 
-		lines := rbtree.BuildLines(tr.Root(), rbtree.SHAPE_SQUARE, 2, true, true)
-		for _, l := range lines {
-			sb.WriteString(l)
+		var sb strings.Builder
+		for _, perm := range perms {
+			strs := make([]string, len(perm))
+			for i, v := range perm {
+				strs[i] = fmt.Sprintf("%s", v)
+			}
+			fmt.Fprintf(&sb, "input: [%s]\n", strings.Join(strs, ","))
+
+			tr := rbtree.NewRBT[string]()
+			for _, v := range perm {
+				tr.Insert(v)
+			}
+
+			lines := rbtree.BuildLines(tr.Root(), shape, 2, true, true)
+			for _, l := range lines {
+				sb.WriteString(l)
+				sb.WriteString("\n")
+			}
 			sb.WriteString("\n")
 		}
-		sb.WriteString("\n")
+
+		if err := os.WriteFile(out, []byte(sb.String()), 0644); err != nil {
+			fmt.Fprintln(os.Stderr, "failed to write output file:", err)
+			os.Exit(1)
+		}
 	}
 
-	if err := os.WriteFile(*out, []byte(sb.String()), 0644); err != nil {
-		fmt.Fprintln(os.Stderr, "failed to write output file:", err)
-		os.Exit(1)
-	}
 }
